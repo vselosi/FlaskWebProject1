9d57a1e9f91b230615172095c23989df54b13904
Roman <romadnestr@gmail.com>
2019-12-28 00:24:22 +0200

    Рефакторинг

--- FlaskWebProject1/HelloFlask/views.py
+++ FlaskWebProject1/HelloFlask/views.py
@@ -1,7 +1,15 @@
-from flask import Flask 
+from flask import Flask, render_template
 from HelloFlask import app
+from datetime import datetime
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return 'Hello Flask'
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d, %B, %Y at %X")
+
+    return render_template(
+        "index.html",
+        title = "Hello Flask",
+        message = "Hello, Flask!",
+        content = " on " + formatted_now)
--- FlaskWebProject1/runserver.py
+++ FlaskWebProject1/runserver.py
@@ -1,22 +1,8 @@
-"""
-This script runs the application using a development server.
-It contains the definition of routes and views for the application.
-"""
+from HelloFlask import app
+import os
 
-from flask import Flask
-app = Flask(__name__)
-
-# Make the WSGI interface available at the top level so wfastcgi can get it.
-wsgi_app = app.wsgi_app
-
-
-@app.route('/')
-def hello():
-    """Renders a sample page."""
-    return "Hello World!"
 
 if __name__ == '__main__':
-    import os
     HOST = os.environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(os.environ.get('SERVER_PORT', '5555'))
